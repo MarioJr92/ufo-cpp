f3f2ee4a70fb29d518a247707f336f96e3d2b248
Mario Rodrigues Jr <102671736+MarioJr92@users.noreply.github.com>
2023-02-20 16:32:00 -0500

    created greet function

---
+++ ufo_functions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Define functions
+void greet()
+{
+  cout << "=============\n";
+  cout << "UFO: The Game\n";
+  cout << "=============\n";
+  cout << "Instructions: save your friend from alien abduction by guessing the letters in the codeword.\n";
+}
